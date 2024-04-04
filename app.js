f7501895e72b39b337db2fd7109e96eaea7b1e8b
HuynhMinhLuan-B2106842 <luanb2106842@student.ctu.edu.vn>
2024-04-04 16:29:49 +0700

    setup project

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const cors = require('cors');
+
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+const authRouter = require('./app/routers/auth.route');
+const bookRouter = require('./app/routers/book.route');
+const userRouter = require('./app/routers/user.route');
+const orderRouter = require('./app/routers/order.route');
+const cartRouter = require("./app/routers/cart.route");
+
+app.get("/", (req, res) =>{
+    res.json({message: "Welcome to my bookstore."});
+});
+
+app.use("/api/auth", authRouter);
+app.use("/api/book", bookRouter);
+app.use("/api/user", userRouter);
+app.use("/api/order", orderRouter);
+app.use("api/cart", cartRouter);
+
+
+
+module.exports = app;
